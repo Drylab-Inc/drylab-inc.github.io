20a358a2527542a0576c42c8b0754e80fc4b5661
Effie Klimi <42475826+effieklimi@users.noreply.github.com>
2024-11-28 18:10:07 +0000

    switching to shadcn theme

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ export default async function Home() {
           </p>
         </a>
         <h1
-          className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-7xl md:leading-[5rem]"
+          className="animate-fade-up bg-gradient-to-br from-foreground to-secondary bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-7xl md:leading-[5rem]"
           style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
         >
           Your research deserves better software
@@ -85,7 +85,7 @@ export default async function Home() {
       </div>
 
       <h1
-        className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text pt-80 text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-5xl md:leading-[5rem]"
+        className="animate-fade-up bg-gradient-to-br from-foreground to-secondary bg-clip-text pt-80 text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-5xl md:leading-[5rem]"
         style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
       >
         Products
@@ -108,7 +108,7 @@ export default async function Home() {
       </div>
 
       <h1
-        className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text pt-80 text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-5xl md:leading-[5rem]"
+        className="animate-fade-up bg-gradient-to-br from-foreground to-secondary bg-clip-text pt-80 text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-5xl md:leading-[5rem]"
         style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
       >
         Contact Us
