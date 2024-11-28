0888981559fe10fa207c72949cbecd22f7c01f27
Effie Klimi <42475826+effieklimi@users.noreply.github.com>
2024-11-28 19:26:08 +0000

    changed footer color

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -3,7 +3,7 @@ import { BuyMeACoffee } from "../shared/icons";
 export default function Footer() {
   return (
     <div className="absolute w-full py-5 text-center">
-      <p className="text-secondary">© Copyright 2024, Drylab Inc.</p>
+      <p className="text-foregrounf/60">© Copyright 2024, Drylab Inc.</p>
     </div>
   );
 }
