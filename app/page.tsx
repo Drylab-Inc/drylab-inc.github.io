4afa15e21fea3f46223d3d837bf4c15adf4d61dd
Effie Klimi <42475826+effieklimi@users.noreply.github.com>
2024-11-28 15:08:29 +0000

    changed cells image to command K

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import ComponentGrid from "@/components/home/component-grid";
 import Image from "next/image";
 import Link from "next/link";
 import { nFormatter } from "@/lib/utils";
-import Cells from "@/public/cells.png";
+import Cells from "@/public/commandK.png";
 import Browser from "@/public/browser.png";
 import Niove from "@/public/niove.png";
 
