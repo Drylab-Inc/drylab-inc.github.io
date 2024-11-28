b2f131588aeb3a3ccb085aae17294b4ae42c4684
Effie Klimi <42475826+effieklimi@users.noreply.github.com>
2024-11-28 00:56:51 +0000

    trying out blog x2

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -7,7 +7,7 @@ import Alert from "@/app/_components/alert";
 import Container from "@/app/_components/container";
 import Header from "@/app/_components/header";
 import { PostBody } from "@/app/_components/post-body";
-import { PostHeader } from "@/app/_components/post-header";
+// import { PostHeader } from "@/app/_components/post-header";
 
 export default async function Post(props: Params) {
   const params = await props.params;
@@ -21,19 +21,19 @@ export default async function Post(props: Params) {
 
   return (
     <main>
-      <Alert preview={post.preview} />
-      <Container>
-        <Header />
-        <article className="mb-32">
-          <PostHeader
-            title={post.title}
-            coverImage={post.coverImage}
-            date={post.date}
-            author={post.author}
-          />
-          <PostBody content={content} />
-        </article>
-      </Container>
+      {/* <Alert preview={post.preview} /> */}
+      {/* <Container> */}
+      <Header />
+      <article className="mb-32">
+        {/* <PostHeader
+          title={post.title}
+          coverImage={post.coverImage}
+          date={post.date}
+          author={post.author}
+        /> */}
+        <PostBody content={content} />
+      </article>
+      {/* </Container> */}
     </main>
   );
 }
