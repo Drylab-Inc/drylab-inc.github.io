85e5ce69022d11b9ef0681d89d922742c205c2ad
Effie Klimi <42475826+effieklimi@users.noreply.github.com>
2024-11-29 20:36:39 +0000

    returned to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,20 +1,20 @@
-import createMDX from "@next/mdx";
-import { withContentlayer } from "next-contentlayer";
+// import createMDX from "@next/mdx";
+// import { withContentlayer } from "next-contentlayer";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // output: "export",
   reactStrictMode: true,
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
-  basePath: "/drylab-inc.github.io",
+  // basePath: "/drylab-inc.github.io",
   swcMinify: true,
   images: {
     domains: ["lh3.googleusercontent.com", "vercel.com"],
     unoptimized: true, // Important for static export
   },
-  experimental: {
-    appDir: true,
-  },
+  // experimental: {
+  //   appDir: true,
+  // },
   async redirects() {
     return [
       {
@@ -26,9 +26,9 @@ const nextConfig = {
   },
 };
 
-// module.exports = nextConfig;
+module.exports = nextConfig;
 
-export default withContentlayer(nextConfig);
+// export default withContentlayer(nextConfig);
 
 // export default withMDX(nextConfig);
 // export  withContentlayer({})
