4d958a349b8680878b5404938d1c3cf3fb702f02
Effie Klimi <42475826+effieklimi@users.noreply.github.com>
2024-11-29 19:45:16 +0000

    updted next.config.mjs x4

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,41 @@
+import createMDX from "@next/mdx";
 import { withContentlayer } from "next-contentlayer";
-import "./env.mjs";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   basePath: "/drylab-inc.github.io",
+  swcMinify: true,
   images: {
     domains: ["lh3.googleusercontent.com", "vercel.com"],
+    unoptimized: true, // Important for static export
+  },
+  experimental: {
+    appDir: true,
+  },
+  async redirects() {
+    return [
+      {
+        source: "/github",
+        destination: "https://github.com/steven-tey/precedent",
+        permanent: false,
+      },
+    ];
   },
 };
 
-// module.exports = nextConfig;
+module.exports = nextConfig;
 
 export default withContentlayer(nextConfig);
+
+// export default withMDX(nextConfig);
+// export  withContentlayer({})
+
+// const nextConfig = {
+//   output: 'export', // This is crucial for static export
+//   basePath: '/your-repo-name', // Replace with your repo name
+//   images: {
+//     unoptimized: true // Important for static export
+//   }
+// }
