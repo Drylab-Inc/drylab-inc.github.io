52bf44b15f2745b2b4a706d41d46a935d851fe3e
Effie Klimi <42475826+effieklimi@users.noreply.github.com>
2024-11-29 02:48:17 +0000

    debugging

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,37 +1,33 @@
 import { getSortedPostsData } from "@/lib/posts";
 
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
-}
-
-// export default async function Home() {
-//   const allPostsData = await getSortedPostsData();
-
-//   return (
-// <Layout home>
-
-// <section>
-//   <h2>Blog</h2>
-{
-  /* <ul>
-        {allPostsData.map(({ id, date, title }) => (
-          <li key={id}>
-            {title}
-            <br />
-            {id}
-            <br />
-            {date}
-          </li>
-        ))}
-      </ul> */
-}
-// </section>
-// </Layout>
-//   );
+// export default function Page() {
+//   return <h1>Hello, Next.js!</h1>;
 // }
 
-// Optional: If you want to add metadata
-// export const metadata = {
-//   title: "Your Home Page Title",
-//   description: "Your home page description",
-// };
+export default async function Home() {
+  const allPostsData = await getSortedPostsData();
+
+  return (
+    <section>
+      <h2>Blog</h2>
+      {
+        <ul>
+          {allPostsData.map(({ id, date, title }) => (
+            <li key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      }
+    </section>
+  );
+}
+
+export const metadata = {
+  title: "Your Home Page Title",
+  description: "Your home page description",
+};
